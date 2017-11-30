28b11b591b4476b77ebe07aaeb839d909ece4c0b
Alexandr Kokorev <ak_78leon@mail.ru>
2017-11-30 22:10:48 +0600

    последний пуш

--- js/script.js
+++ js/script.js
@@ -3,6 +3,12 @@ var feedbackPopup = document.querySelector(".modal--feedback");
 var feedbackClose = document.querySelector(".modal__close");
 var feedbackName = feedbackPopup.querySelector("[name=user-name]");
 var overlay = document.querySelector(".overlay");
+var searchLink = document.querySelector(".user-menu__item--search");
+var searchPopup = document.querySelector(".form--search");
+var searchName = searchPopup.querySelector("[name=search]");
+var loginLink = document.querySelector(".user-menu__link--login");
+var loginPopup = document.querySelector(".form--login");
+var loginEmail = loginPopup.querySelector("[name=user-email]");
 
 
 feedbackLink.addEventListener ("click", function(event) {
@@ -17,6 +23,16 @@ feedbackClose.addEventListener ("click", function(event) {
   feedbackPopup.classList.remove("modal-show");
   overlay.classList.remove("overlay-show");
 })
+
+searchLink.addEventListener("mouseover", function(event) {
+  event.preventDefault();
+  searchName.focus();
+});
+
+loginLink.addEventListener("mouseover", function(event) {
+  event.preventDefault();
+  loginEmail.focus();
+});
 
 window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
